54df1f12272c12a9f30efd4d6783e3e9a2c8deea
Matt Mitchell <goodieboy@gmail.com>
2010-03-01 20:25:12 -0500

    added autoload for Blacklight::App::Controller::Catalog

--- lib/blacklight/app.rb
+++ lib/blacklight/app.rb
@@ -1,6 +1,7 @@
 module Blacklight::App
   module Controller
     autoload :Application, 'blacklight/app/controller/application'
+    autoload :Catalog, 'blacklight/app/controller/catalog'
   end
   module App
     autoload :Application, 'blacklight/app/helper/application'
