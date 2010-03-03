c06f796acc1b48997bc251627b0269df0419dad9
William Butler <tachyonwill@gmail.com>
2010-03-03 12:53:31 -0500

    More work on tests

--- features/step_definitions/folder_steps.rb
+++ features/step_definitions/folder_steps.rb
@@ -1,5 +1,5 @@
 Then /^I should see an add to folder form for ckey "([^\"]*)"$/ do |arg1|
-  response.should have_tag("div#doc_#{arg1} form.addFolderForm")
+  response.should have_tag("input#submitFolderForm_#{arg1}")
 end
 
 When /^I add ckey "([^\"]*)" to my folder$/ do |arg1|
@@ -8,10 +8,13 @@ end
 
 Given /^I have ckey "([^\"]*)" in my folder$/ do |arg1|
   visit catalog_path(arg1)
-  click_link("Add to Marked List")
+  click_button("submitFolderForm_#{arg1}")
   click_link("Marked List")
 end
-
+Given /^that I have cleared my folder$/ do
+  click_link("Marked List")
+  click_link("Clear Marked List")
+end
 #Given /^I am logged in$/ do
 #  visit root_path
 #  click_link("Login using NetBadge")
