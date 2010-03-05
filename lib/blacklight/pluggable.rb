52767b5070e8f6136798ab9cfc997a0bad49ecdb
Matt Mitchell <goodieboy@gmail.com>
2010-03-05 12:09:28 -0500

    more docs for plugin

--- lib/blacklight/pluggable.rb
+++ lib/blacklight/pluggable.rb
@@ -6,31 +6,35 @@ module Blacklight::Pluggable
     @plugins ||= []
   end
   
-  # use extend to bring-in plugin functionality:
+  # plugin
+  # 
+  # use extend to use Blacklight::Pluggable
+  # 
   # class TheClass
   #   extend Blacklight::Pluggable
-  #   plugin MyModule
+  #   plugin MyModule, my_opts = {}
   # end
   # 
   # The module (MyModule) passed to #plugin can:
-  #   - have a self.configure(klass, opts) method, which can be used to setup the class (TheClass)
-  #     - klass the class calling plugin (TheClass in the example above)
+  #   - have a self.configure(klass, opts) method, which can be used to setup the class (TheClass in the example above)
+  #     - klass is the class calling plugin (TheClass in the example above)
+  #     - opts is an options hash passed into #plugin (my_opts in the example above)
   #   - contain an InstanceMethods module (MyModule::InstanceMethods)
   #   - contain a ClassMethods module (MyModule::ClassMethods)
   # 
-  # #plugin also accepts an options hash which can contain
-  # any options -- these are passed to the plugin modules' self.configure method
+  # #plugin also accepts an options hash which can contain any options
+  # -- these are passed to the plugin modules' self.configure method
   # ... but there are 2 special keys ...
   #   :instance_methods and :class_methods
   # Both of thses options are evaluated as boolean values.
   # 
   # :class_methods
-  # If :class_methods => true, the ClassMethods module is included into the calling class
-  # If :class_methods => false, the ClassMethods module is NOT included into the calling class
+  # If :class_methods => true, the MyModule::ClassMethods module is included into the calling class
+  # If :class_methods => false, the MyModule::ClassMethods module is NOT included into the calling class
   # 
   # :instance_methods
-  # If :instance_methods => true, the InstanceMethods module is extended into the calling class
-  # If :instance_methods => false, the InstanceMethods module is NOT extended into the calling class
+  # If :instance_methods => true, the MyModule::InstanceMethods module is extended into the calling class
+  # If :instance_methods => false, the MyModule::InstanceMethods module is NOT extended into the calling class
   def plugin mod, opts = {}
     opts = {:class_methods => true, :instance_methods => true}.merge(opts)
     extend mod::ClassMethods if opts[:class_methods] and mod.const_defined?(:ClassMethods)
